1ffe6c310bc848d2c0a2faa1248b15590d5939e6
PaulVirally <pvirally@gmail.com>
2017-09-01 15:56:34 -0400

    Added reproduce function

--- Chromosome.py
+++ Chromosome.py
@@ -15,7 +15,7 @@ class Chromosome(object):
         self._genes = genes
         self._fitness = float('-inf')
 
-    def mutate(self, mutation_rate=0.001):
+    def _mutate(self, mutation_rate=0.001):
         """Mutates the choromosome to add some randomness to the evolution
         process.
         
@@ -25,7 +25,7 @@ class Chromosome(object):
         Args:
             mutation_rate: The probability that any bit of any of the
             chromosome's genes will flip (default: 0.001). The reason this is 
-            a parameter of this function and not a paramter of the constructor
+            a parameter of this function and not a parameter of the constructor
             of the class, is to allow a varying mutation rate which might be 
             desired in some situations.
         """
@@ -53,15 +53,83 @@ class Chromosome(object):
         After crossover:  AAAA bbbb cccc DDDD
 
         Args:
-            other: The other chromosome that will give its DNA to the offspring
+            other: The other chromosome that will give its DNA to the
+                   offspring.
             crossover_rate: The probability that a new crossover point will be
-                            set at any set of genes (default: 0.3)
+                            set at any set of genes (default: 0.3).
 
         Returns:
             This returns a new chromosome which is the "offspring" of this
             chromosome and `other`. This new chromosome contains a genetic
-            sequence that is inherited from both "parents"
+            sequence that is inherited from both "parents".
         """
-        longest, shortest = sorted([self._genes, other._genes],
-                key=lambda x: len(x), reverse=True)
+        longest, shortest = sorted([self._genes, other._genes], key=lambda x: len(x), reverse=True)
+        longests_turn = True
+        new_genes = []
 
+        # Give the appropriate parent's genes to the offspring
+        for i in range(len(longest)):
+            if longests_turn:
+                new_genes.append(longest[i])
+            else:
+                # If the shortest has already run out of genes, ignore it
+                try:
+                    new_genes.append(shortes[i])
+                except IndexError:
+                    pass
+
+            # Change turn with `crossover_rate` probability
+            if random() <= crossover_rate:
+                longests_turn = not longests_turn
+
+        return Chromosome(new_genes)
+
+    def get_dna(self):
+        """Returns the DNA of the individual chromosome.
+        
+        This functions returns the DNA of the chromosome in a slightly
+        human-readable manner (just a string).
+        
+        Returns:
+            The DNA of the chromosome.
+        """
+        return ''.join(self._genes)
+
+    def reproduce(self, other, crossover_func, crossover_rate=0.3, mutation_rate=0.001)
+        """Performs crossover and then mutation to the new born chromosome.
+        
+        This function performs crossover and then mutation to a new born
+        chromosome that is then returned. Once you have found two chromosomes
+        to reproduce with, this is the function to be called to make a new
+        chromosome that inherits genes from both its "parents".
+
+        Args:
+            other: The other chromosome that will give its DNA to the
+                   offspring.
+            crossover_func: A string representing the function that will
+                            perform the crossover. See the other member
+                            functions for a full list of available crossover
+                            functions.
+            crossover_rate: The probability that a new crossover point will be
+                            set at any set of genes (default: 0.3).
+            mutation_rate: The probability that any bit of any of the
+                           chromosome's genes will flip (default: 0.001). The
+                           reason this is a parameter of this function and not
+                           a parameter of the constructor of the class, is to
+                           allow a varying mutation rate which might be desired
+                           in some situations.
+
+        Returns:
+            A new chromosome that is the offpsring of `self` and `other`. This
+            new offspring also has gone through mutation and so is fully
+            prepared to go through a fitness test.
+        """
+        # Crossover
+        if (crossover_func == "multipoint by gene"):
+            self._multipoint_crossover_by_gene(other, crossover_rate)
+        else:
+            print("Unknown crossover function `{0}`. Aborting.".format(crossover_func))
+            return
+
+        # Mutate
+        self._mutate(mutation_rate)
