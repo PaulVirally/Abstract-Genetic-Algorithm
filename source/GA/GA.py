ed856cd154ce2013a8e90f55b337387a78e44ac7
Paul Virally <pvirally@gmail.com>
2019-08-16 16:33:47 -0700

    Added GA class

--- source/GA/GA.py
+++ source/GA/GA.py
@@ -1,11 +1,28 @@
+from .Chromosome import Chromosome
+from .Crossover import single_point
+from .Selection import roulette_wheel
+
 class GA:
-
-    def __init__(self, chromo_builder, fitness_eval, pop_size):
-
-        self._chromo_builder = chromo_builder # TODO: Accept chromo_builder args and kwargs
+    def __init__(self, population, fitness_eval, **kwargs):
         self._fitness_eval = fitness_eval # TODO: Accept fitness_eval args and kwargs
-        self._pop_size = pop_size
+        
+        self._pop = population
+        self._pop_size = len(self._pop)
+        self._fitnesses = [float('-inf')]*self._pop_size # TODO: Make fitness part of the Chromosome class?
 
-        self.pop = []
+        self._selection = kwargs.get('selection', roulette_wheel)
+        self._num_parents = kwargs.get('num_parents', 2)
+
+        self._crossover = kwargs.get('crossover', single_point)
+
+    def step(self):
+        new_pop = []
+        new_fitnesses = []
         for _ in range(self._pop_size):
-            self.pop.append(_chromo_builder())
+            parents = self._selection(self._pop, self._fitnesses, self._num_parents)
+            baby = self._crossover(parents)
+            baby.mutate()
+            new_pop.append(baby)
+            new_fitnesses.append(self._fitness_eval(baby))
+        self._pop = new_pop
+        self._fitnesses = new_fitnesses
